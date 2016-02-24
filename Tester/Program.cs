0b68b2768f330032adabba6923b04dda84ef5674
Carda <cagdas.arda@live.com>
2016-02-24 20:44:15 +0200

    first load

---
+++ Tester/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using GenericParser.ParserWorker;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+
+
+namespace Tester
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string FileNamestr = @"C:\hede\TRAN0630";
+            long fileLength = (new FileInfo(FileNamestr)).Length;
+            byte[] buff = new byte[fileLength];
+            using (MemoryMappedFile mmf = MemoryMappedFile.CreateFromFile(FileNamestr, FileMode.Open, "Text", 0, MemoryMappedFileAccess.Read))
+            {
+                using (var reader = mmf.CreateViewAccessor(0, 0, MemoryMappedFileAccess.Read))
+                {
+                    reader.ReadArray<byte>(0, buff, 0, (int)fileLength);
+                }
+            }
+
+            ParseIt.Instance.ConvertFromText<TranObj>(buff, 4724, 6627960, 135);
+        }
+    }
+}
---
+++ Tester/TranObj.cs
@@ -0,0 +1,16 @@
+﻿using GenericParser.ParserAttributes;
+
+namespace Tester
+{
+    [ParserClass(ParserClazz = true)]
+    class TranObj
+    {
+        [ParserFieldProperties(BaslangicIndex = 12, Uzunluk = 8)]
+        public string CicsName { get; set; }
+        [ParserFieldProperties(BaslangicIndex = 21, Uzunluk = 4)]
+        public string TranId { get; set; }
+        [ParserFieldProperties(BaslangicIndex = 58, Uzunluk = 5, OndalikAyiraci = ".")]
+        public double AverageResponseTime { get; set; }
+
+    }
+}
